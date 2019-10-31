08b524e41de5208b467ab104601b8b3161be937c
sojeongLee19 <55444000+sojeongLee19@users.noreply.github.com>
2019-10-31 17:03:40 +0900

    9_5

--- main.c
+++ main.c
@@ -1,20 +1,53 @@
 #include <stdio.h>
 #include <stdlib.h>
+#define ROWS        3
+#define COLS        3 
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
+void printMatrix(int a[][COLS]){
+	int i, j;
+	for (i=0;i<ROWS;i++){
+	     for (j=0;j<COLS;j++)
+	          printf("%i", a[i][j]);
+	     printf("\n");
+	}
+}
+void addMatrix(int a[][COLS],int b[][COLS],int c[][COLS])
+{
+	int i, j;
+	for (i=0;i<ROWS;i++)
+	     for (j=0;j<COLS;j++)
+	         c[i][j] = a[i][j]+b[i][j];	
+	
+}
+
 int main(void) 
 { 
-    int i;
-    int grade[5]; 
-    printf("5명의 점수를 입력하세요. \n"); 
-     
-	for(i=0; i<5; i++)
-	 scanf("%d", &grade[i]); 
+int a[ROWS][COLS] = {  
+    {2, 3, 0},  
+    {8, 9, 1},  
+    {7, 0, 5} };
 
-     for( i=0; i<5; i++) 
-     printf("grade[%d] = %d\n", i, grade[i]); 
-     
-     
-     return 0;
+ int b[ROWS][COLS] = {  
+    {1, 0, 0},   
+	{0, 1, 0},   
+	{0, 0, 1} }; 
+	int c[ROWS][COLS];          
+
+addMatrix(a, b, c); 
+
+printMatrix(c); 
+
+return 0; 
 }
+
+
+
+
+
+
+
+
+
+
