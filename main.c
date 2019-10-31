808b36337d0ed6464977f47d21dc9a8a114b60a7
sojeongLee19 <55444000+sojeongLee19@users.noreply.github.com>
2019-10-31 15:43:11 +0900

    09_2

--- main.c
+++ main.c
@@ -3,19 +3,18 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-int main(int argc)
-{
-
+int main(void) 
+{ 
     int i;
-    int a[5] = {1, 2, 3, 4, 5}; 
-    int b[5] = {1, 2, 3, 4, 5};
+    int grade[5]; 
+    printf("5명의 점수를 입력하세요. \n"); 
+     
+	for(i=0; i<5; i++)
+	 scanf("%d", &grade[i]); 
 
-     for (i=0; i<5; i++)
-      {
-          if(a[i]! = b[i])
-	       {
-		  printf("%i is different\n",i);
-	       }
-      }
-    return 0;
+     for( i=0; i<5; i++) 
+     printf("grade[%d] = %d\n", i, grade[i]); 
+     
+     
+     return 0;
 }
